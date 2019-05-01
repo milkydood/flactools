7c665a9260ec7ab8a3e58503531285c5b34d9ee2
Andrew White <andrew.white@team.telstra.com>
2019-05-01 12:21:04 +1000

    rewrite

--- listinadequatepictures.sh
+++ listinadequatepictures.sh
@@ -1,30 +1,13 @@
 #!/bin/bash
-TMP=$(mktemp)
-
-# Just a small script to extract images from flac files, and list any
-# flacs that don't at least have an image of 300x300
-# - probably doesn't work too great with multiple images.
-
-function tidy() {
-	rm -rf $TMP
-}
-
-trap tidy QUIT INT EXIT
 
 for flacpath in "$@"; do
-	for file in $(find $flacpath -type f -name '*flac'); do
-		cp /dev/null $TMP
-		metaflac --export-picture-to=$TMP $file
-	# this sort of works in bash 4.4+
-	#	mapfile -d ';' -t D <<<$(identify -format "%w;%h" $TMP)
-	# this works, but below read is more bullet proof
-	#IFS="x" read W H  <<<$(identify -format "%wx%h" $TMP)
-	# this works, but just IFS is simpler
-		read -d '' -ra D <<<$(identify -format "%w\n%h" $TMP)
-		if [ ${D[0]} -lt 300 -o ${D[1]} -lt 300 ]; then
-			echo "$file (${D[0]}x${D[1]})"
-	#	if [ ! $W -ge 300 -a $H -ge 300 ]; then
-	#		echo "$file (${W}x$H)"
-		fi
-	done
+        while IFS= read -r -d '' file; do
+                read -d '' -ra D <<<$(metaflac --list "$file" 2>/dev/null |strings | grep -Po '(width|height)\: \K.*')
+
+                if [ "${D[0]}" == "" -o "${D[1]}" == "" ]; then
+                        echo "$file (No picture?)"
+                elif [ ${D[0]} -lt 300 -o ${D[1]} -lt 300 ]; then
+                        echo "$file (${D[0]}x${D[1]})"
+                fi
+        done < <(find $flacpath -type f -name '*flac' -print0)
 done
